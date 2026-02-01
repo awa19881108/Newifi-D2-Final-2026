d1aa5a71dc7c14f0e8a7b2a889a0e7c4f6ed1033
awa19881108 <awa19881108@gmail.com>
2026-02-02 03:46:58 +0800

    Add diy-part1.sh for feeds
    This script modifies the feeds.conf.default file by removing existing entries for helloworld, mosdns, and passwall, then adds new sources for these packages.

---
+++ diy-part1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 簡單版 diy-part1.sh，加 helloworld + mosdns + passwall2
+
+sed -i '/helloworld/d' feeds.conf.default
+sed -i '/mosdns/d' feeds.conf.default
+sed -i '/passwall/d' feeds.conf.default
+
+echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+
+cat feeds.conf.default
