561fe86535f5df421ec03c30f0a27eebafb586b1
awa19881108 <awa19881108@gmail.com>
2026-02-02 22:38:24 +0800

    Switch all feeds to GitHub mirror to avoid 503

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-# 簡單版 diy-part1.sh，加 helloworld + mosdns + passwall2
+# 使用 GitHub 鏡像來源，避免 git.openwrt.org 503
 
-sed -i '/helloworld/d' feeds.conf.default
-sed -i '/mosdns/d' feeds.conf.default
-sed -i '/passwall/d' feeds.conf.default
+# 官方 feed 用 GitHub 鏡像
+echo "src-git packages https://github.com/openwrt/packages.git;openwrt-24.10" > feeds.conf.default
+echo "src-git luci https://github.com/openwrt/luci.git;openwrt-24.10" >> feeds.conf.default
+echo "src-git routing https://github.com/openwrt-routing/packages.git;openwrt-24.10" >> feeds.conf.default
+echo "src-git telephony https://github.com/openwrt/telephony.git;openwrt-24.10" >> feeds.conf.default
 
+# 自訂 feed 保持原樣（GitHub 來源）
 echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> feeds.conf.default
-git clone --depth=1 -b openwrt-24.10 https://github.com/openwrt/openwrt.git openwrt
-# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
-# 升級 golang 到 24.x（修 geoview / xray 等 Go 包版本需求）
-rm -rf feeds/packages/lang/golang
-git config --global http.postBuffer 1048576000
-git config --global http.version HTTP/1.1
-git config --global core.compression 0
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
-rm -rf feeds/helloworld/geoview
-rm -rf feeds/helloworld/daed
-rm -rf feeds/helloworld/v2ray-core
-
+# golang 升級到 24.x（修 geoview / xray 版本）
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
