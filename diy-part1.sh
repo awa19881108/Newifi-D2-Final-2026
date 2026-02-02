4bca0e5c1cb81253dedf9b03e07d448ee6f724e1
awa19881108 <awa19881108@gmail.com>
2026-02-02 21:54:26 +0800

    Modify feeds.conf.default and clone repositories
    Updated feed configuration and cloned necessary repositories.

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,10 +6,20 @@ sed -i '/mosdns/d' feeds.conf.default
 sed -i '/passwall/d' feeds.conf.default
 
 echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> feeds.conf.default
-echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
+git clone --depth=1 -b openwrt-24.10 https://github.com/openwrt/openwrt.git openwrt
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 # 升級 golang 到 24.x（修 geoview / xray 等 Go 包版本需求）
+rm -rf feeds/packages/lang/golang
+git config --global http.postBuffer 1048576000
+git config --global http.version HTTP/1.1
+git config --global core.compression 0
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+rm -rf feeds/helloworld/geoview
+rm -rf feeds/helloworld/daed
+rm -rf feeds/helloworld/v2ray-core
+
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
