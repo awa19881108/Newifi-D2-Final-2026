51e1b143bd64492c8cf9ff5fe8187e4454439b38
awa19881108 <awa19881108@gmail.com>
2026-02-02 21:18:55 +0800

    Upgrade golang to version 24.x
    Upgrade Go to version 24.x and update feeds.

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,4 +9,8 @@ echo "src-git helloworld https://github.com/sbwml/openwrt_helloworld.git^v5" >> 
 echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git^v5" >> feeds.conf.default
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
+# 升級 golang 到 24.x（修 geoview / xray 等 Go 包版本需求）
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
 cat feeds.conf.default
